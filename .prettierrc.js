41fcb1e91c379bc5a3cd8d2b9a37f510312a7f27
Djordje Stanojevic <djordje.stanojevic@braintribe.com>
2020-04-25 13:23:12 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    packageManager: 'yarn',
+    printWidth: 120
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="hello">
+        <p>
+            <label for="name">Lion:</label>
+            <input id="name" v-model="msg.lion.eats" />
+        </p>
+        <p>
+            <label for="name">Rabbit:</label>
+            <input id="name" v-model="msg.rabbit.eats" />
+        </p>
+        <p>Lion: {{ msg.lion.eats }}</p>
+        <p>Rabbit: {{ msg.rabbit.eats }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: ['msg']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>

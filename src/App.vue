0dacab6ae2eaabd36aaea70c2b19fc361cdf9b22
Djordje Stanojevic <djordje.stanojevic@braintribe.com>
2020-04-25 13:07:52 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld :msg="testData" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            testData: null
+        }
+    },
+    created() {
+        // test starts
+        let animal = {
+            eats: 'eats'
+        }
+
+        let rabbit = {
+            jumps: { value: 'jumps' }
+        }
+
+        Object.setPrototypeOf(rabbit, animal)
+        rabbit.pera = Object.getPrototypeOf(rabbit)
+
+        let lion = Object.create(animal)
+
+        this.testData = {
+            rabbit: rabbit,
+            lion: lion
+        }
+        // test ends;
+    }
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
